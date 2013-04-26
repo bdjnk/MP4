27df68ab8392024402c5d516c230a0b646e798ae
Michael Plotke <michaelplotke@gmail.com>
2013-04-26 16:38:02 -0700

    More animation tinkering

--- Assets/Scripts/HeroBehavior.cs
+++ Assets/Scripts/HeroBehavior.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 
 public class HeroBehavior : MonoBehaviour {
 	
+    // Speed controls
+    private const float speed = 20f;
+	private float rotateSpeed = Mathf.PI / 2f; // 90-degrees in 2 seconds
+	
 	private SpriteSheetManager spriteManager = null;
 	public int mRowOfSpriteElements = 0;
 	public int mColumnOfSpriteElements = 0;
@@ -13,27 +17,24 @@ public class HeroBehavior : MonoBehaviour {
 	public int mBlankPixelsToBottom = 0;
 	
 	// per action sprite animation information 
-	private const float walkPeriod = 0.25f; // update interveral in seconds
-	private const float runPeriod = 0.1f;
+	private const float animationSpeed = speed / 80; // (0.25f) update interveral in seconds
 	
 	// Row, beginColumn, endColumn. Columns are read right to left.
-	private SpriteActionDefinition walkUp = new SpriteActionDefinition(0, 2, 0, walkPeriod, true); 
-	private SpriteActionDefinition walkDown = new SpriteActionDefinition(0, 0, 2, walkPeriod, true);
+	private SpriteActionDefinition walkUp = new SpriteActionDefinition(0, 2, 0, animationSpeed, true); 
+	private SpriteActionDefinition walkDown = new SpriteActionDefinition(1, 0, 2, animationSpeed, true);
 	
-	private SpriteActionDefinition walkLeft = new SpriteActionDefinition(0, 1, 0, walkPeriod, true);
-	private SpriteActionDefinition walkRight = new SpriteActionDefinition(0, 1, 2, walkPeriod, true);
+	private SpriteActionDefinition walkLeft = new SpriteActionDefinition(0, 1, 0, animationSpeed, true);
+	private SpriteActionDefinition walkRight = new SpriteActionDefinition(0, 1, 2, animationSpeed, true);
 	
-	private SpriteActionDefinition stop = new SpriteActionDefinition(0, 1, 1, walkPeriod, false); // to stop
+	private SpriteActionDefinition stop = new SpriteActionDefinition(2, 2, 2, animationSpeed, false); // to stop
+	
+	private float previousDirection = 0f; // this is stupid...
 	
 	// to support time ...
-	private float preEggSpawnTime = -1f; // 
+	private float preEggSpawnTime = -1f;
 	private float eggSpawnInterval = 0.1f; // in seconds
 	
 	private GameObject eggPrefab = null;
-	
-    // Speed controls
-    private float speed = 20f;
-	private float rotateSpeed = Mathf.PI / 2f; // 90-degrees in 2 seconds
 	
 	private GlobalBehavior worldScript;
 	
@@ -63,18 +64,19 @@ public class HeroBehavior : MonoBehaviour {
 		// Must call to see the sprite movement!
 		spriteManager.UpdateSpriteAnimation();
 		
-		if (Input.GetKeyDown(KeyCode.UpArrow)) {
-			spriteManager.SetSpriteAnimationAciton(walkUp);
+		if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow)) {
+			//if (Input.GetKeyDown(KeyCode.UpArrow) && !Input.GetKey(KeyCode.DownArrow)) {
+			if (previousDirection <= 0 && Input.GetAxis("Vertical") > 0) {
+				spriteManager.SetSpriteAnimationAciton(walkUp);
+			}
+			//if (Input.GetKeyDown(KeyCode.DownArrow) && !Input.GetKey(KeyCode.UpArrow)) {
+			if (previousDirection >= 0 && Input.GetAxis("Vertical") < 0) {
+				spriteManager.SetSpriteAnimationAciton(walkDown);
+			}
 		}
-		if (Input.GetKeyDown(KeyCode.DownArrow)) {
-			spriteManager.SetSpriteAnimationAciton(walkDown);
-		}	
-		if (Input.GetKeyUp(KeyCode.UpArrow)) {
+		else {
 			spriteManager.SetSpriteAnimationAciton(stop);
 		}
-		if (Input.GetKeyUp(KeyCode.DownArrow)) {
-			spriteManager.SetSpriteAnimationAciton(stop);
-		}	
 		
 		if (Input.GetAxis ("Fire1") > 0f) { // this is Left-Control
 			
@@ -92,5 +94,6 @@ public class HeroBehavior : MonoBehaviour {
 				}
 			}
 		}
+		previousDirection = Input.GetAxis("Vertical");
 	}
 }
